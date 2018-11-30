45b53694e2ab58a5533047b22a2c034cf87791db
nturinski <naturins@microsoft.com>
2018-11-30 14:38:12 -0800

    new project

--- BlobTriggerJS/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = async function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
-};
